24aa14964c999b9e98fc668bdefac2536f8fb140
Jesses Cemac Ukaegbu <85634164+JessesCUkaegbu@users.noreply.github.com>
2022-12-14 12:31:24 +0000

    first commit made

---
+++ rock-paper-scissorgame/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Header from './component/header';
+import Body from "./component/body"
+
+function App() {
+  return (
+     <>
+      <Header></Header>
+      <Body></Body>
+     </>
+  );
+}
+
+export default App;
---
+++ rock-paper-scissorgame/src/component/header.js
@@ -0,0 +1,11 @@
+import react from "react";
+
+export default function Header() {
+  return (
+    <>
+      <header className="header">
+        <h3>Rock Paper Scissor</h3>
+      </header>
+    </>
+  );
+}
